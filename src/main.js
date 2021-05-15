60a33b9df3cf21ea768b1c2429e615a9f56fa937
Alexander Bird <alex@alexanderbird.software>
2021-05-15 15:32:26 -0700

    Add basic styling; auto-select the uuid
    Color palette: https://colorhunt.co/palette/273174

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { v4 } from 'uuid';
 
+function selectAll(element) {
+  const selection = window.getSelection();
+  if (selection.toString() == '') {
+    setTimeout(function(){
+      const range = document.createRange();
+      range.selectNodeContents(element);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    });
+  }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('#uuid').innerHTML = v4();
+  const element = document.querySelector('#uuid');
+  element.innerHTML = v4();
+  selectAll(element);
+  setTimeout(() => {
+    element.innerHTML = v4();
+    selectAll(element);
+  }, 50 * 1000);
 });
--- www/bundle.js
+++ www/bundle.js
@@ -75,8 +75,26 @@
     return stringify(rnds);
   }
 
+  function selectAll(element) {
+    const selection = window.getSelection();
+    if (selection.toString() == '') {
+      setTimeout(function(){
+        const range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+      });
+    }
+  }
+
   window.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#uuid').innerHTML = v4();
+    const element = document.querySelector('#uuid');
+    element.innerHTML = v4();
+    selectAll(element);
+    setTimeout(() => {
+      element.innerHTML = v4();
+      selectAll(element);
+    }, 50 * 1000);
   });
 
 }());
