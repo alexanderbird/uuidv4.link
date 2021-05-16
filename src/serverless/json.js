1127b4f7f1502e95059f0e0669199ed301f6da4f
Alexander Bird <alex@alexanderbird.software>
2021-05-16 00:11:34 -0700

    Explicitly set http content type headers for txt & json

--- src/serverless/json.js
+++ src/serverless/json.js
@@ -4,6 +4,9 @@ import { v4 } from 'uuid';
 exports.handler = (event, context) => {
   return {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(v4()),
   };
 };
--- src/serverless/txt.js
+++ src/serverless/txt.js
@@ -4,6 +4,9 @@ import { v4 } from 'uuid';
 exports.handler = (event, context) => {
   return {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
     body: v4(),
   };
 };
